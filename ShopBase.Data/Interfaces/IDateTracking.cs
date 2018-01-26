940682d4fc3dbe5a77d261ac989d53e7372dcefc
Minhvh <minhvh107@gmail.com>
2018-01-26 17:44:45 +0700

    2

---
+++ ShopBase.Data/Interfaces/IDateTracking.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShopBase.Data.Interfaces
+{
+    public interface IDateTracking
+    {
+        DateTime DateCreated { set; get; }
+        DateTime DateModified { set; get; }
+    }
+}
