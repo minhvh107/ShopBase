d8e92d72aeae324739ab047b30bccb6f8a2224f3
Minhvh <minhvh107@gmail.com>
2018-01-26 16:44:04 +0700

    Add project files.

---
+++ ShopBase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
